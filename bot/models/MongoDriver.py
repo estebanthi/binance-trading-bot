a6b0dfeab9dc6bb19f31d2790eab1c5317017a4b
estebanthi <77675611+estebanthi@users.noreply.github.com>
2021-11-23 14:31:42 +0100

    Update MongoDriver.py

--- bot/models/MongoDriver.py
+++ bot/models/MongoDriver.py
@@ -1,14 +1,25 @@
 import pymongo
 import yaml
-import datetime as dt
 
 
 class MongoDriver:
+    """
+    Class to interact with MongoDB database
+
+    You have to configure it in config.yml file
+    Check README for more details
+
+    """
 
     def __init__(self):
         self.client = None
 
     def connect(self):
+        """
+        Connect to database
+
+        """
+
         with open("config.yml", "r") as file:
             url = yaml.safe_load(file)["mongo_url"]
         client = pymongo.MongoClient(url)
@@ -17,28 +28,52 @@ class MongoDriver:
         self.client = client
 
     def add_ticker(self, ticker, timeframe, historical):
+        """
+        Add a ticker to database
+
+        """
+
         db = self.client.tradingbot
         collection = db.Tickers
         collection.insert_one({"symbol": ticker, "timeframe": timeframe, "historical": historical})
 
     def update_ticker(self, ticker, timeframe, historical):
+        """
+        Update a ticker in database
+
+        """
+
         db = self.client.tradingbot
         collection = db.Tickers
 
-        collection.update({"symbol": "BTC/EUR"}, {"$set": {"historical": historical}})
+        collection.update({"symbol": ticker, "timeframe": timeframe}, {"$set": {"historical": historical}})
 
     def get_historical(self, ticker, timeframe):
+        """
+        Get an historical from database
+
+        """
+
         db = self.client.tradingbot
         collection = db.Tickers
 
         return collection.find_one({"symbol": ticker, "timeframe": timeframe})["historical"]
 
     def get_ticker(self, ticker, timeframe):
+        """
+        Get a ticker from database
+
+        """
+
         db = self.client.tradingbot
         collection = db.Tickers
 
         return collection.find_one({"symbol": ticker, "timeframe": timeframe})
 
     def close(self):
+        """
+        Close connection
+
+        """
         self.client.close()
         print("MongoDriver disconnected")
